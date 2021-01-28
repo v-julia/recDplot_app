868526c64dcbdf7843eecc00bb388cb4648d4d2f
unknown <vjulia94@gmail.com>
2021-01-28 12:58:19 +0300

    added progress to fig from paper

--- app.R
+++ app.R
@@ -891,54 +891,71 @@ server <- function(input, output) {
     #df = l_fig[[2]]
     
     if (f==3){
-      l_fig = plot_dist_test(aln_fig, min1,max1,min2,max2)
-      df = l_fig[[2]]
-      output$fig3_plot <- renderPlot(l_fig[[1]])
-      observeEvent(input$fig3_brush, {
-        brushed_points3 <- brushedPoints(df, input$fig3_brush)
-        min_1 = input$fig3_brush$xmin
-        max_1 = input$fig3_brush$xmax
+      withProgress(message = 'Creating distance plots', value = 0, {
         
-        min_2 = input$fig3_brush$ymin
-        max_2 = input$fig3_brush$ymax
-        output$min_max <- DT::renderDataTable(brushed_points3)
-        
-        output$fig3_brush_info <- DT::renderDataTable({
-          find_recomb_names(l_fig[[3]], min_1, max_1, l_fig[[4]], min_2, max_2)
+        aln_control =  cbind(aln_fig[,min1:max1], aln_fig[,min2:max2])
+        output$fig3_control <- renderPlot({
+          plot_control(aln_control)
         })
+        incProgress(0.4, detail = "Control plot finished")
+        Sys.sleep(0.5)
+        
+        l_fig = plot_dist_test(aln_fig, min1,max1,min2,max2)
+        df = l_fig[[2]]
+        incProgress(0.4, detail = "Distance plot finished")
+        Sys.sleep(0.5)
+        
+        output$fig3_plot <- renderPlot(l_fig[[1]])
+        
+        
+        observeEvent(input$fig3_brush, {
+          brushed_points3 <- brushedPoints(df, input$fig3_brush)
+          min_1 = input$fig3_brush$xmin
+          max_1 = input$fig3_brush$xmax
+          
+          min_2 = input$fig3_brush$ymin
+          max_2 = input$fig3_brush$ymax
+          output$min_max <- DT::renderDataTable(brushed_points3)
+          
+          output$fig3_brush_info <- DT::renderDataTable({
+            find_recomb_names(l_fig[[3]], min_1, max_1, l_fig[[4]], min_2, max_2)
+          })
+        })
+        incProgress(0.2, detail = "Creating plots")
       })
-      aln_control =  cbind(aln_fig[,min1:max1], aln_fig[,min2:max2])
-      output$fig3_control <- renderPlot({
-        plot_control(aln_control)
-      }
-      
-      
-      )
-      
       
     }
     if (f==4){
-      l_fig = plot_dist_test(aln_fig, min1,max1,min2,max2)
-      df = l_fig[[2]]
-      output$fig4_plot <- renderPlot(l_fig[[1]])
-      observeEvent(input$fig4_brush, {
-        brushed_points4 <- brushedPoints(df, input$fig4_brush)
-        min_1 = input$fig4_brush$xmin
-        max_1 = input$fig4_brush$xmax
-        
-        min_2 = input$fig4_brush$ymin
-        max_2 = input$fig4_brush$ymax
-        output$min_max <- DT::renderDataTable(brushed_points4)
-        
-        output$fig4_brush_info <- DT::renderDataTable({
-          find_recomb_names(l_fig[[3]], min_1, max_1, l_fig[[4]], min_2, max_2)
-        })
-      })
-      aln_control =  cbind(aln_fig[,min1:max1], aln_fig[,min2:max2])
-      output$fig4_control <- renderPlot({
-        plot_control(aln_control)
-      })
+      withProgress(message = 'Creating distance plots', value = 0, {
       
+        aln_control =  cbind(aln_fig[,min1:max1], aln_fig[,min2:max2])
+        output$fig4_control <- renderPlot({
+          plot_control(aln_control)
+        })
+        incProgress(0.4, detail = "Control plot finished")
+        Sys.sleep(0.5)
+        
+        l_fig = plot_dist_test(aln_fig, min1,max1,min2,max2)
+        df = l_fig[[2]]
+        incProgress(0.4, detail = "Distance plot finished")
+        Sys.sleep(0.5)
+        
+        output$fig4_plot <- renderPlot(l_fig[[1]])
+        observeEvent(input$fig4_brush, {
+          brushed_points4 <- brushedPoints(df, input$fig4_brush)
+          min_1 = input$fig4_brush$xmin
+          max_1 = input$fig4_brush$xmax
+          
+          min_2 = input$fig4_brush$ymin
+          max_2 = input$fig4_brush$ymax
+          output$min_max <- DT::renderDataTable(brushed_points4)
+          
+          output$fig4_brush_info <- DT::renderDataTable({
+            find_recomb_names(l_fig[[3]], min_1, max_1, l_fig[[4]], min_2, max_2)
+          })
+        })
+        incProgress(0.2, detail = "Creating plots")
+      })
     }
   
   }
